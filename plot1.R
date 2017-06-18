e547b0ad65f9c89af4206af660a3e8a4bf5101db
Jeff Grady <jeff@dogearsoftware.com>
2017-06-18 09:39:49 -0400

    First draft of Plot 1. X axis might need some tweaking.

---
+++ plot1.R
@@ -0,0 +1,11 @@
+elec <- read.csv2("household_power_consumption.txt")
+foo <- subset(elec, as.Date(Date, format = "%m/%d/%Y") %in%
+                  c(as.Date("2007-02-01"), as.Date("2007-02-02")))
+hist(as.numeric(as.character(foo$Global_active_power)),
+     breaks = 12,
+     xlim = c(0,6),
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+dev.copy(png,'plot1.png', width = 480, height = 480)
+dev.off()
